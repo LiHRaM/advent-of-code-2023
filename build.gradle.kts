6f27d50d1d0d6a9f7ad47e9fb59bdcca14edc9c9
Hilmar Gústafsson <LiHRaM@users.noreply.github.com>
2023-12-17 12:44:18 +0100

    feat: remove amper, it does not support structure

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,10 @@ plugins {
     kotlin("jvm") version "1.9.21"
 }
 
+dependencies {
+    implementation("org.junit.jupiter:junit-jupiter:5.8.1")
+}
+
 sourceSets {
     main {
         kotlin.srcDir("src")
